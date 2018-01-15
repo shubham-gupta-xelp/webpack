55adb58e4d71b6552b7e1de51f7c7483d08ce19d
Shubham <shubham@xelpmoc.in>
2018-01-15 16:22:52 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }]
+            }
+
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+
+        })
+    ]
+}
